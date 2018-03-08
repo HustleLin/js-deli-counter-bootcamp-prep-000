727ce2dc1685e48ce9784b485dd1e2d129fdcbb9
Jonathan Lin <jlin8293@gmail.com>
2018-03-08 01:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var katzDeliLine = [];
-  if (line.length == 0) {
+  if (katzDeliLine.length == 0) {
     return("The line is currently empty.")
   }
   else {
